faefe669e38eb55f39baf0b7667153528dc6089b
csw <zobirofkir19@gmail.com>
2025-11-03 04:22:28 +0100

    create register new user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\RegisterResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * Register New User
+     */
+    public function register(RegisterRequest $request) : RegisterResource
+    {
+        return RegisterResource::make(
+            User::create(
+                $request->validated()
+            )
+        );
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::prefix('auth')->group(function() {
+    Route::post('register', [AuthController::class , 'register']);
+});
