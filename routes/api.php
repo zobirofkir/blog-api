8003bf8fab0ddc452e932627ca2fbd1b17dc9b26
csw <zobirofkir19@gmail.com>
2025-11-04 00:59:41 +0100

    create logout current auth user

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function() {
     Route::post('register', [AuthController::class , 'register']);
     Route::post('login', [AuthController::class , 'login']);
+});
 
+Route::prefix('user')->middleware('auth:api')->group(function() {
+    Route::post('logout', [AuthController::class, 'logout']);
 });
